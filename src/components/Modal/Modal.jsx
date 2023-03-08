39c1ce0375c0c3793e091416d79b8c50c38064d0
AleksandrSoiko <farshchef777@gmail.com>
2023-03-08 23:06:19 +0000

    search and modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,44 @@
 import PropTypes from 'prop-types';
 import { Overlay, Modal } from './Modal.styled';
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
+import { useEffect } from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class ModalWindow extends Component {
-  static propTypes = {
-    selectedImage: PropTypes.string,
-    tags: PropTypes.string,
-    onClose: PropTypes.func,
-  };
+export const ModalWindow = props => {
+  useEffect(() => {
+    window.addEventListener('keydown', handlePressEsc);
+    return () => {
+      window.removeEventListener('keydown', handlePressEsc);
+    };
+  }, []);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handlePressEsc);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handlePressEsc);
-  }
-
-  handlePressEsc = event => {
+  function handlePressEsc(event) {
     if (event.code === 'Escape') {
-      this.props.onClose();
+      props.onClose();
     }
-  };
-
-  handleClickBackdrop = event => {
-    if (event.target === event.currentTarget) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    const { selectedImage, tags } = this.props;
-
-    return createPortal(
-      <Overlay onClick={this.handleClickBackdrop}>
-        <Modal>
-          <img src={selectedImage} alt={tags} />
-        </Modal>
-      </Overlay>,
-      modalRoot
-    );
   }
-}
+
+  function handleClickBackdrop(event) {
+    if (event.target === event.currentTarget) {
+      props.onClose();
+    }
+  }
+
+  const { selectedImage, tags } = props;
+
+  return createPortal(
+    <Overlay onClick={handleClickBackdrop}>
+      <Modal>
+        <img src={selectedImage} alt={tags} />
+      </Modal>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+ModalWindow.propTypes = {
+  selectedImage: PropTypes.string,
+  tags: PropTypes.string,
+  onClose: PropTypes.func,
+};
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Formik, Field } from 'formik';
+import { Formik } from 'formik';
 import {
   Header,
   Form,
@@ -9,53 +9,39 @@ import {
   SearchButtonLabel,
   Input,
 } from './SearchBar.styled';
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class SearchBar extends Component {
-  state = {
-    searchQuery: '',
+export const SearchBar = props => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleChange = event => {
+    setSearchQuery(event.currentTarget.value.toLowerCase());
   };
 
-  handleChange = event => {
-    this.setState({
-      searchQuery: event.currentTarget.value.toLowerCase().trim(),
-    });
-  };
-
-  handleSubmit = () => {
-    if (this.state.searchQuery.trim() === '') {
+  const handleSubmit = () => {
+    if (searchQuery.trim() === '') {
       toast.error('Please enter the word');
       return;
     }
-    this.props.onSubmit(this.state.searchQuery);
+    props.onSubmit(searchQuery);
   };
-
-  CustomInputComponent = props => (
-    <Input
-      type="text"
-      name="searchQuery"
-      placeholder="Search images and photos"
-      onChange={this.handleChange}
-    />
+  return (
+    <Header>
+      <Formik initialValues={searchQuery} onSubmit={handleSubmit}>
+        <Form>
+          <FormField>
+            <Input
+              type="text"
+              name="searchQuery"
+              placeholder="Search images and photos"
+              onChange={handleChange}
+            />
+          </FormField>
+          <SearchButton type="submit">
+            <SearchButtonLabel>Search</SearchButtonLabel>
+          </SearchButton>
+        </Form>
+      </Formik>
+    </Header>
   );
-
-  render() {
-    return (
-      <Header>
-        <Formik
-          initialValues={{ searchQuery: '' }}
-          onSubmit={this.handleSubmit}
-        >
-          <Form>
-            <FormField>
-              <Field as={this.CustomInputComponent} />
-            </FormField>
-            <SearchButton type="submit">
-              <SearchButtonLabel>Search</SearchButtonLabel>
-            </SearchButton>
-          </Form>
-        </Formik>
-      </Header>
-    );
-  }
-}
+};
